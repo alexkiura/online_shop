b30ca66639c2de9f1aa19ae8dd2bf200472a0dbc
Alex Kiura <kiuraalex@gmail.com>
2017-01-24 08:29:03 +0300

    chore: register models on admin site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+"""Configurations for admin site."""
 
-# Register your models here.
+from django.contrib import admin
+from shop.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin site confifguration for Category model."""
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+class ProductAdmin(admin.ModelAdmin):
+    """Admin site configuration for Product model."""
+    list_display = ['name', 'slug', 'price', 'stock',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
