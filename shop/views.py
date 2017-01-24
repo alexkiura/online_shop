3ea38dd2e723e766de81df02c178b9b89a79e57a
Alex Kiura <kiuraalex@gmail.com>
2017-01-24 19:08:37 +0300

    feat: add product views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+"""Application views."""
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from shop.models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    """Render a list of products in the store."""
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    """Render a single product."""
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detaul.html',
+                  {'product': product})
