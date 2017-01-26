d8a5c2a5bbb61576f4b65604e9a8c0c33610c550
Alex Kiura <kiuraalex@gmail.com>
2017-01-26 12:15:06 +0300

    chore: style image on product list

--- myshop/settings.py
+++ myshop/settings.py
@@ -41,6 +41,10 @@ ROOT_URLCONF = 'myshop.urls'
 
 MEDIA_URL = '/media/'
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 TEMPLATES = [
