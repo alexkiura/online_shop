5c47e6d9ce0c1527e1b6c9c5882ebb91aa951a5e
Alex Kiura <kiuraalex@gmail.com>
2017-02-14 11:41:16 +0300

    chore: add production settings

---
+++ myshop/settings/production.py
@@ -0,0 +1,11 @@
+"""Production settings."""
+import dj_database_url
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
