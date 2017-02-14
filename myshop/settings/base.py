e1301791a91879333786245260998423360b65e2
Alex Kiura <kiuraalex@gmail.com>
2017-02-14 11:41:45 +0300

    chore: add test settings

--- myshop/settings/base.py
+++ myshop/settings/base.py
@@ -1,12 +1,12 @@
 """
 Django settings for myshop project.
 """
-
 import os
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'm4ardrbu!=_rvqr#5=^0*fyz(q9ih^pqo$in&wdl@*nf0ksluu'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
@@ -67,13 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myshop.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
