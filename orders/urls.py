523deea5aac740b6d1e0a521e8ab0a41710fc7bf
Alex Kiura <kiuraalex@gmail.com>
2017-03-17 02:03:48 +0300

    chore: add create order urls

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+"""Urls for order application."""
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^create/$',
+        views.order_create,
+        name='order_create'),
+]
