4ade84364fca3bdb164d0beeea167f847ceadfa9
Alex Kiura <kiuraalex@gmail.com>
2017-04-05 11:27:09 +0300

    feat: send emails on order creation

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.helper import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 
 
 def order_create(request):
@@ -18,6 +19,8 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
