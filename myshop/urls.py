4181f76ee2f8325a294426c79b57ec5c0d716099
Alex Kiura <kiuraalex@gmail.com>
2017-04-20 19:25:00 +0300

    chore: add paypal urls

--- myshop/urls.py
+++ myshop/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^paypal/', include('paypal.standard.ipn.urls')),
     url(r'^', include('shop.urls', namespace='shop')),
     url(r'^orders/', include('orders.urls', namespace='orders')),
 ]
